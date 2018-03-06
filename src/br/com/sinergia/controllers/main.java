4c419f7babf1a59af478b1108368710f6724a097
marceloev <marcelo.sinergia@outlook.com>
2018-03-06 14:06:58 -0300

    Anotação
    Anotação

--- src/br/com/sinergia/controllers/main.java
+++ src/br/com/sinergia/controllers/main.java
@@ -6,6 +6,9 @@ import br.com.sinergia.models.TabelaFull;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+
 public class main extends Application {
 
     public static void main(String[] args) {
@@ -15,9 +18,8 @@ public class main extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            //ReaderDBDic.getTabelaFullByIndex(0);
-            Campo campoComplemento = ReaderDBDic.getCampo(0, 2);
-            System.out.println(campoComplemento.getNomeCampo());
+            System.out.println(Timestamp.from(Instant.now()));
+            System.out.println(Timestamp.from(Instant.now()));
         } catch (Exception ex) {
             System.err.println(ex.getMessage());
             ex.printStackTrace();
